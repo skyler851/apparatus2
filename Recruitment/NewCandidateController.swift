93d29521cdceadf370f2cd2269032a4498032f63
Stephanie <skyler@purdue.edu>
2015-03-28 13:51:18 -0400

    Name, email, and date enter in parse

--- Recruitment/NewCandidateController.swift
+++ Recruitment/NewCandidateController.swift
@@ -11,6 +11,42 @@ import UIKit
 class NewCandidateController: UIViewController {
     @IBOutlet weak var menuButton:UIBarButtonItem!
     
+    //submit new candidate info
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var gradDate: UIDatePicker!
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    
+    //enter submit button
+    @IBAction func submit(sender: AnyObject) {
+       
+        //get what user typed
+        var usrEntered = usernameTextField.text
+        var emlEntered = emailTextField.text
+        var gradEntered = gradDate.date
+        
+        //make sure name and email are filled out
+        if usrEntered != "" && emlEntered != "" && gradEntered != "" {
+            
+            //get the call Candidate from parse and enter the data into the columns
+            var object = PFObject(className: "Candidates")
+            object.setObject(usrEntered , forKey: "name")
+            object.setObject(emlEntered, forKey: "email")
+            object.setObject(gradEntered, forKey: "gradDate")
+            object.save()
+            
+        } else {
+            
+            //message if name and email are not filled out
+            self.messageLabel.text = "All Fields Required"
+        }
+        
+    }
+    //end submit stuff
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,3 +76,32 @@ class NewCandidateController: UIViewController {
     */
     
 }
+
+
+//get user input
+/*var usrEntered = usernameTextField.text
+var emlEntered = emailTextField.text
+
+var user = PFUser()
+user.username = usrEntered
+user.email = emlEntered
+
+func userSignUp(){
+    user.signUpInBackgroundWithBlock {
+        (succeeded: Bool!, error: NSError!) -> Void in
+        if error == nil {
+            // Hooray! Let them use the app now.
+            self.messageLabel.text = "User Signed Up";
+        } else {
+            // Show the errorString somewhere and let the user try again.
+        }
+    }
+}
+
+//if name and email are not null
+if usrEntered != "" && emlEntered != "" {
+    userSignUp()
+} else {
+    self.messageLabel.text = "All Fields Required"
+}
+*/
