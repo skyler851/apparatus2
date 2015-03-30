46656639669e28ea4d0082fe91d1526a167b55af
Stephanie <skyler@purdue.edu>
2015-03-29 20:09:38 -0400

    added New Candidate functionality to enter info into Parse

--- Recruitment/NewCandidateController.swift
+++ Recruitment/NewCandidateController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+var name = ""
+var status = ""
 
 class NewCandidateController: UIViewController {
     @IBOutlet weak var menuButton:UIBarButtonItem!
@@ -15,34 +17,89 @@ class NewCandidateController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var gradDate: UIDatePicker!
+    @IBOutlet weak var jobType: UISegmentedControl!
     
-    @IBOutlet weak var messageLabel: UILabel!
+    //perform segue to go to thank you page
+    override func shouldPerformSegueWithIdentifier(identifier: String!, sender: AnyObject!) -> Bool {
+        
+        //check if segue identifier is toThankYouPage
+        if identifier == "toThankYouPage"{
+            
+            if (emailTextField.text.isEmpty) {
+                //alert empty email
+                let alert = UIAlertView()
+                alert.title = "Invalid Email"
+                alert.message = "Please enter your email address"
+                alert.addButtonWithTitle("Ok")
+                alert.show()
+                
+                return false
+            }
+            
+            if status == "false"{
+                //alert that email already exists
+                let alert = UIAlertView()
+                alert.title = "Email Already Exists"
+                alert.message = "Please let the recruiter know that your email is already in our system"
+                alert.addButtonWithTitle("Ok")
+                alert.show()
+                
+                return false
+            }
+            
+            if status == "true"{
+                performSegueWithIdentifier("toThankYouPage", sender: self)
+                return true
+            }
+        }
+        return true
+    }
     
     
     //enter submit button
-    @IBAction func submit(sender: AnyObject) {
+    @IBAction func submit(sender: UIBarButtonItem) {
        
         //get what user typed
         var usrEntered = usernameTextField.text
         var emlEntered = emailTextField.text
         var gradEntered = gradDate.date
+        var jobEntered = jobType.selectedSegmentIndex
+        var jobToParse = ""
         
-        //make sure name and email are filled out
-        if usrEntered != "" && emlEntered != "" && gradEntered != "" {
+        if (jobEntered == 0){ jobToParse = "Full-Time May"}
+        else if (jobEntered == 1){jobToParse = "Full-Time Dec"}
+        else {jobToParse = "Internship"}
+        
+        var query = PFQuery(className:"Candidates")
+        query.whereKey("email", equalTo: emlEntered )
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]!, error: NSError!) -> Void in
+        
+            //make sure name and email are filled out
+            if usrEntered != "" && emlEntered != "" && gradEntered != "" || error == nil {
+                
+                if (objects.count == 0) {
+                    //get the call Candidate from parse and enter the data into the columns
+                    var object = PFObject(className: "Candidates")
+                    object.setObject(usrEntered , forKey: "name")
+                    object.setObject(emlEntered, forKey: "email")
+                    object.setObject(gradEntered, forKey: "gradDate")
+                    object.setObject(jobToParse, forKey: "jobType")
+                    object.save()
+                    status = "true"
+                }
+                
+                if (objects.count != 0) {
+                    //email already exists, do not push segue
+                    status = "false"
+                }
+            }
             
-            //get the call Candidate from parse and enter the data into the columns
-            var object = PFObject(className: "Candidates")
-            object.setObject(usrEntered , forKey: "name")
-            object.setObject(emlEntered, forKey: "email")
-            object.setObject(gradEntered, forKey: "gradDate")
-            object.save()
-            
-        } else {
-            
-            //message if name and email are not filled out
-            self.messageLabel.text = "All Fields Required"
+            else {
+                println("Error: \(error) \(error.userInfo!)")
+            }
+            self.shouldPerformSegueWithIdentifier("toThankYouPage", sender: self)
         }
-        
     }
     //end submit stuff
 
