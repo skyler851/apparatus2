6ca9e5d0b3366560908243b048ab9f495c7926e3
Stephanie <skyler@purdue.edu>
2015-04-05 15:20:15 -0400

    merged Candidate List

---
+++ Recruitment/UniversityListTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  UniversityListTableViewController.h
+//  Recruitment
+//
+//  Created by Sean VanPelt on 4/4/15.
+//  Copyright (c) 2015 AppAtUs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Parse/Parse.h>
+#import "UniversityListTableViewCell.h"
+
+NSArray *UniversitySelected;
+
+@interface UniversityListTableViewController : UITableViewController
+
+@end
