e2905aae317305be88d5b069356eea68a8cd33e6
Stephanie <skyler@purdue.edu>
2015-04-04 21:09:06 -0400

    added events page. wont display university name on label

---
+++ Recruitment/EventsCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  EventsCollectionViewCell.h
+//  Recruitment
+//
+//  Created by Stephanie Kyler on 4/3/15.
+//  Copyright (c) 2015 AppAtUs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EventsCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UILabel *EventsLabel;
+
+@end
---
+++ Recruitment/EventsCollectionViewCell.m
@@ -0,0 +1,45 @@
+//
+//  EventsCollectionViewCell.m
+//  Recruitment
+//
+//  Created by Stephanie Kyler on 4/3/15.
+//  Copyright (c) 2015 AppAtUs. All rights reserved.
+//
+
+#import "EventsCollectionViewCell.h"
+
+@implementation EventsCollectionViewCell
+
+- (void)awakefromnib {
+    //Code for changing the background of the cell when you select it.
+    
+    /*
+     //Not Selected
+     UIView *bgView = [[UIView alloc]initWithFrame:self.bounds];
+     self.backgroundView = bgView;
+     self.backgroundView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"blue"]];
+     
+     //Not Selected
+     UIView *selectedView = [[UIView alloc]initWithFrame:self.bounds];
+     self.selectedBackgroundView = selectedView;
+     self.selectedBackgroundView.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"red"]];
+     */
+    
+    
+    //Not Selected
+    /*
+     UIView *bgView = [[UIView alloc]initWithFrame:self.bounds];
+     self.backgroundView = bgView;
+     self.backgroundView.backgroundColor = [UIColor blueColor];
+     */
+    
+    //Selected
+    /*
+     UIView *selectedView = [[UIView alloc]initWithFrame:self.bounds];
+     self.selectedBackgroundView = selectedView;
+     self.selectedBackgroundView.backgroundColor = [UIColor redColor];
+     */
+    
+}
+
+@end
