8b1fee11c3b616e8186d3521aa18b92518277c6c
Stephanie <skyler@purdue.edu>
2015-04-08 12:56:58 -0400

    changes to storyboard

---
+++ Recruitment/AdminMenuController.swift
@@ -0,0 +1,12 @@
+//
+//  AdminMenuController.swift
+//  Recruitment
+//
+//  Created by Stephanie Kyler on 4/2/15.
+//  Copyright (c) 2015 AppAtUs. All rights reserved.
+//
+
+import UIKit
+
+class AdminViewController: UIViewController {
+}
