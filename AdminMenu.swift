29f62d141c0b4f33a48ca92fa9a0430b61846d68
Stephanie <skyler@purdue.edu>
2015-04-08 19:18:23 -0400

    added views to storyboard

--- AdminMenu.swift
+++ AdminMenu.swift
@@ -1,9 +1,30 @@
 import UIKit
 
-class AdminMenu: UIViewController {
+
+class AdminMenuController: UITableViewController{
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+    @IBOutlet weak var lblRecruiterName: UIBarButtonItem!
     
-    override func viewDidLoad() {
+
+        override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //display recruiter name in toolbar
+        lblRecruiterName.title = name
+        
+        //menu
+        if self.revealViewController() != nil {
+            menuButton.target = self.revealViewController()
+            menuButton.action = "revealToggle:"
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        }
     }
-   
+    
+    //passes the employee name to next contoller
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
 }
