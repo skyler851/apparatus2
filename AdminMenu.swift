16a734fe3348372a7dbebea83f75688afd8ffb07
Stephanie <skyler@purdue.edu>
2015-04-12 12:56:52 -0400

    New branch from master with Events

--- AdminMenu.swift
+++ AdminMenu.swift
@@ -1,30 +1,9 @@
 import UIKit
 
-
-class AdminMenuController: UIViewController{
-    @IBOutlet weak var menuButton:UIBarButtonItem!
-    @IBOutlet weak var lblRecruiterName: UIBarButtonItem!
+class AdminMenu: UIViewController {
     
-
-        override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //display recruiter name in toolbar
-        lblRecruiterName.title = name
-        
-        //menu
-        if self.revealViewController() != nil {
-            menuButton.target = self.revealViewController()
-            menuButton.action = "revealToggle:"
-            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        }
     }
-    
-    //passes the employee name to next contoller
-    
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 }
--- Recruitment/Recruitment-Bridging-Header.h
+++ Recruitment/Recruitment-Bridging-Header.h
@@ -6,10 +6,18 @@
 //  Copyright (c) 2015 AppAtUs. All rights reserved.
 //
 
-
 #import "SWRevealViewController.h"
-
 #import "SkillsCollectionViewController.m"
-#import "ParseUI/ParseUI.h"
+
+//SkillResult - NSString
+#import "EventsCollectionViewController.m"
+//UniversitiesSelected  - NSArray
+//EventSelected         - NSArray
+//EventTimeSelected     - NSArray
+
+#import "CandidateListTableViewCell.h"
+#import "CandidateListTableViewController.h"
 #import <Parse/Parse.h>
-// or #import <ParseOSX/ParseOSX.h>
+#import "SummaryDetailViewController.h"
+
+
